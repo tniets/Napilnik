8a3e95bc718592f028c23e5e24f28c26d2dc4e19
tniets <artem.bernshteyn@gmail.com>
2021-11-20 18:17:57 +0300

    Completed task 1

---
+++ Task1/Bot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Napilnik.Task1
+{
+    public class Bot
+    {
+        private readonly Weapon _weapon;
+
+        public Bot(Weapon weapon)
+        {
+            _weapon = weapon ?? throw new NullReferenceException(nameof(weapon));
+        }
+
+        public void OnSeePlayer(Player player)
+        {
+            _weapon.Fire(player);
+        }
+    }
+}
---
+++ Task1/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Napilnik.Task1
+{
+    public class Player
+    {
+        private int _health;
+
+        public Player(int health)
+        {
+            if (health < 1)
+                throw new ArgumentOutOfRangeException(nameof(health));
+
+            _health = health;
+        }
+
+        private bool IsDead => _health == 0;
+
+        public void TakeDamage(int damage)
+        {
+            if (IsDead)
+                throw new InvalidOperationException();
+            
+            if (damage < 0)
+                throw new ArgumentOutOfRangeException(nameof(damage));
+            
+            _health = Math.Clamp(_health - damage, 0, int.MaxValue);
+        }
+    }
+}
