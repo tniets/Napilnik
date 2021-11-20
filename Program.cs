dccc9931901c368761ffa028ac5adbb1f0d03ebf
tniets <artem.bernshteyn@gmail.com>
2021-11-20 17:17:05 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Napilnik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
